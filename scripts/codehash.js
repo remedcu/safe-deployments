632a101f87f063e377bbd9f5fa0c0ff03f41d04d
Shebin John <admin@remedcu.com>
2024-07-04 11:35:57 +0200

    Review Script Checking 22

--- scripts/codehash.js
+++ scripts/codehash.js
@@ -4,3 +4,7 @@ const defaultAddress = process.env.DEFAULTADDRESS
 const rpc = process.env.RPCURL
 
 exec('echo $(cast keccak $(cast code '+defaultAddress+' --rpc-url '+rpc+') > codehash.txt)');
+exec('echo "Address: '+defaultAddress+'"');
+exec('echo "RPC: '+rpc+'"');
+exec('echo "Code hash: "');
+exec('cat codehash.txt');
